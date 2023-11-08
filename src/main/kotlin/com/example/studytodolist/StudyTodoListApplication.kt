094e87911813d7f24d8be3c03762b2555a1115e3
Y-Joo <wldudwn99@gmail.com>
2023-11-08 19:46:22 +0900

    Initial Commit

---
+++ src/main/kotlin/com/example/studytodolist/StudyTodoListApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studytodolist
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudyTodoListApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudyTodoListApplication>(*args)
+}
