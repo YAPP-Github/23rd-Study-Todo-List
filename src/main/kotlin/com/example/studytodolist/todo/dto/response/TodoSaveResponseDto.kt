034c610270313b4b3b0f93ff1a447e2ff00584f3
Y-Joo <wldudwn99@gmail.com>
2023-11-13 15:43:27 +0900

    Fix: Fixed response dto

--- src/main/kotlin/com/example/studytodolist/todo/dto/response/TodoSaveResponseDto.kt
+++ src/main/kotlin/com/example/studytodolist/todo/dto/response/TodoSaveResponseDto.kt
@@ -4,9 +4,10 @@ import com.example.studytodolist.todo.domain.Progress
 import com.example.studytodolist.todo.domain.Todo
 
 data class TodoSaveResponseDto(
+    val id: Long,
     val title: String,
     val content: String,
     val progress: Progress
 ){
-    constructor(todo: Todo): this(todo.title, todo.content, todo.progress)
+    constructor(todo: Todo): this(todo.id, todo.title, todo.content, todo.progress)
 }
--- src/main/kotlin/com/example/studytodolist/todo/dto/response/TodoUpdateResponseDto.kt
+++ src/main/kotlin/com/example/studytodolist/todo/dto/response/TodoUpdateResponseDto.kt
@@ -4,8 +4,9 @@ import com.example.studytodolist.todo.domain.Progress
 import com.example.studytodolist.todo.domain.Todo
 
 data class TodoUpdateResponseDto(
+    val id: Long,
     val title: String,
     val progress: Progress
 ){
-    constructor(todo: Todo): this(todo.title, todo.progress)
+    constructor(todo: Todo): this(todo.id, todo.title, todo.progress)
 }
