ec9ca705aab78133b5c8f60338469598dd90e88c
seokyeong <purewater71@naver.com>
2023-11-13 18:16:54 +0900

    [Config] initial setting

---
+++ src/test/java/site/yapp/study/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package site.yapp.study.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
