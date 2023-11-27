74378e0d5275c9893dbc292754f650bf627b2322
Y-Joo <wldudwn99@gmail.com>
2023-11-28 01:00:42 +0900

    Chore: Added test code

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,10 @@ dependencies {
     implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.0.4")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.7.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.7.2")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
+    testImplementation("io.mockk:mockk:1.13.8")
 }
 
 tasks.withType<KotlinCompile> {
