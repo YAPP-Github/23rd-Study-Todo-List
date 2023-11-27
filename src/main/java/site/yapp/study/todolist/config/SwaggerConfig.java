fa039088f7e1ac5d901bf91a4bd96899b47855bf
Seokyeong <purewater71@naver.com>
2023-11-28 03:17:22 +0900

    [Feat] connecting swagger

---
+++ src/main/java/site/yapp/study/todolist/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package site.yapp.study.todolist.config;
+
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("TodoList API")
+                        .version("1.0.0")
+                        .description("TodoList API Docs"));
+    }
+}
